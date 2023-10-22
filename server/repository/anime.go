0747ccbd28664c9c05190e28f2404753e1e556e8
taikicoco <kawasakitaiki0308@gmail.com>
2023-10-22 17:18:00 +0900

    add:anime data seed

--- server/repository/anime.go
+++ server/repository/anime.go
@@ -3,22 +3,19 @@ package repository
 import (
 	"context"
 	"server/model"
-	// "github.com/jmoiron/sqlx"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type AnimeRepository struct{}
 
-func (ar *AnimeRepository) GetAll(ctx context.Context) ([]*model.Anime, error) {
-	animes := []*model.Anime{
-		{
-			AnimeID: 1,
-			Name:    "anime1",
-		},
-		{
-			AnimeID: 2,
-			Name:    "anime2",
-		},
+func (ar *AnimeRepository) GetAll(ctx context.Context, db *sqlx.DB) ([]*model.Anime, error) {
+	animes := []*model.Anime{}
+	err := db.SelectContext(ctx, &animes,
+		"SELECT anime_id, name FROM animes")
+
+	if err != nil {
+		return nil, err
 	}
 	return animes, nil
-
 }
